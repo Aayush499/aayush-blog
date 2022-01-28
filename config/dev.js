ee734aa08552cb93ce8db5cb9b2dd888fe6c0290
Aayush499 <partypooper24>
2022-01-28 13:29:47 +0530

    move private info to dev.js

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+   mongoURI: 'mongodb+srv://Aayush-Bhandari:aryaman24@aayush-blog.zrzy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const express  = require('express');
-const app  = express();
-
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Aayush-Bhandari:aryaman24@aayush-blog.zrzy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-{useNewUrlParser: true}).then(()=> console.log('DB connected'))
-                        .catch(err=>console.error(err));
-                                     
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-app.get('/', (req,res)=>{
-    res.send("Hello World")
-});
+const config = require('./config/key');
 
-app.listen(5000);    
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
+     {useNewUrlParser: true }).then(() =>console.log('DB connected'))
+                              .catch(err => console.error(err));
+
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.post('/api/users/register', (req, res) => {
+        const user = new User(req.body);
+
+        user.save()
+    .then(() => res.status(200).json(`User '${req.body.firstname}' added!`))
+    .catch((err) => res.status(400).json(`Unable to add user. Error: ${err}.`));
+
+})
+
+app.get('/',(req,res)=> {
+    res.send({"Hello":"Hi"})
+})
+app.listen(5000);
