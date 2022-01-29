61be6c50f606a1ad31567bc95989d4c25e3deb2e
Aayush499 <partypooper24>
2022-01-30 02:23:42 +0530

    final ?????

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,21 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+import {Provider} from "react-redux";
+import {createStore , applyMiddleware} from "redux";
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from "./reducers";
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
+  <Provider store = {createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
   <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </BrowserRouter>
+  </Provider>
   ,
   document.getElementById('root')
 );
